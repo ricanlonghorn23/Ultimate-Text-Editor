6e83f05f83fad5ddeec73f5afbf8aa6ff197565c
ricanlonghorn23 <luis.ra.aldaz@gmail.com>
2024-07-02 20:02:02 -0500

    update readme

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -1,6 +1,6 @@
 const butInstall = document.getElementById('buttonInstall');
 
-// Logic for installing the PWA
+
 window.addEventListener('beforeinstallprompt', (event) => {window.deferredPrompt = event;
     butInstall.classList.toggle('hidden', false);
 });
